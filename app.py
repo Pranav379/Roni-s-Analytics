e1a40c0e9cd59ac1ebe28ffe582d6f369d006519
Pranav379 <pranavb07@gmail.com>
2024-12-12 14:36:24 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+
+import data
+
